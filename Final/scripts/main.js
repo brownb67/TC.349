39854743fd519862af4d313f43db3f6d3703a613
brownb67 <brownbritani21@gmail.com>
2013-12-08 23:59:05 -0500

    Final

---
+++ Final/scripts/main.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+
+function pressMock()
+{
+var content = document.getElementById("frame");
+content.src="mock.html";
+}
+function pressStory2()
+{
+var content = document.getElementById("frame");
+content.src="pages/story2.html";
+}
+
+function pressStory1()
+{
+var content = document.getElementById("frame");
+content.src = "pages/story1.html";
+}
+
+function pressSeasons()
+{
+var content = document.getElementById("frame");
+content.src = "pages/seasons.html";
+}
+
+function pressCard()
+{
+var content = document.getElementById("frame");
+content.src = "pages/cards.html";
+}
+
+ function pressEnd()
+{
+var content = document.getElementById("frame");
+content.src = "pages/End.html";
+}
